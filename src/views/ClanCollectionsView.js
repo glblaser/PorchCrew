02d8c93686b734b22544e19e2901df75da489c73
Gary Blaser <glblaser@gmail.com>
2020-03-19 01:27:56 -0500

    updated date function to use month name

--- src/views/ClanCollectionsView.js
+++ src/views/ClanCollectionsView.js
@@ -3,97 +3,86 @@ import m from 'mithril'
 export const ClanCollectionsView = ({ attrs: { clan, warClient }}) => {
   let collections = []
 
-  const renderCollectionsTableHead = () => {
-    const thAttrs = (heading) => {
-      return {
-        class: 'player',
-        // tabindex: '0',
-        // 'aria-controls': 'collectionsTable',
-        // rowspan: '1',
-        // colspan: '1',
-        // 'aria-label': `${heading}: activate to sort column ascending`,
-        // style: 'width: 125px'
-      }
-    }
-    
-    const renderDateHeaders = () => {
-      const dates = []
-
-      if (collections[0]) {
-        for (let i=0; i<10; i++) {
-          let date = new Date(collections[i].createdDate)
-          date = date.getMonth()+1 + '/' + date.getDate()
-          dates.push(m('th', thAttrs(date), date))
-        }
-      }
-
-      return dates
-    }
-
-    return m('thead',
-      m('tr',  
-        m('th', thAttrs('Player'), 'Player'),
-        renderDateHeaders(),
-        m('th', thAttrs('Total'), 'Total')
-      )
-    )
-  }
-
-  const renderCollectionsTableBody = () => {
-
-    const renderCollectionsRows = () => {
-      const rows = []
-      const clanTotals = [0,0,0,0,0,0,0,0,0,0]
-
-      if (collections[0]) {
-        for (let index=0; index<collections.length; index+=10) {
-          const row = [m('td.player', collections[index].name)]
-          let total = 0
-
-          for (let i=index; i<index+10; i++) {
-            const cardsEarned = collections[i].cardsEarned
-            
-            total += cardsEarned
-            clanTotals[i-index] += cardsEarned
-            row.push(m('td', cardsEarned))
-          }
-          total = total != 0 ? total : null
-          rows.push(m('tr', row, m('td.total', total)))
-        }
-
-        const totalsRow = [m('td', 'Total')]
-        clanTotals.forEach((ele) => {
-          totalsRow.push(m('td', ele))
-        })
-
-        const totalTotals = clanTotals.reduce((a, b) => a + b, 0)
-        totalsRow.push(m('td', totalTotals))
-
-        rows.push(m('tr#collectionTotals.total', totalsRow))
-      }
-
-      return rows 
-    }
-
-    return m('tbody', renderCollectionsRows())
-  }
-
   const renderCollectionsTable = () => {
-    return m('table', {
-      id: 'collectionsTable',
-      class: 'table table-striped',
-      style: 'width:100%'
-      },
-      renderCollectionsTableHead(),
-      renderCollectionsTableBody()
-    )
-  
+
+    const renderCollectionsTableHead = () => {
+      const renderDateHeaders = () => {
+        const dates = []
+
+        for (let i=0; i<10; i++) {
+          const date = new Date(collections[i].createdDate)
+          const month = date.toLocaleString('default', { month: 'short' })
+          const day = date.getDate() 
+          dates.push(m('th', month + ' ' + day))
+        }
+
+        return dates
+      }
+
+      return m('thead',
+        m('tr',  
+          m('th', 'Player'),
+          renderDateHeaders(),
+          m('th', 'Total')
+        )
+      )
+    }
+
+    const renderCollectionsTableBody = () => {
+
+      const renderCollectionsRows = () => {
+        const rows = []
+        const clanTotals = [0,0,0,0,0,0,0,0,0,0]
+
+        if (collections[0]) {
+          for (let index=0; index<collections.length; index+=10) {
+            const row = [m('td.player', collections[index].name)]
+            let total = 0
+
+            for (let i=index; i<index+10; i++) {
+              const cardsEarned = collections[i].cardsEarned
+              
+              total += cardsEarned
+              clanTotals[i-index] += cardsEarned
+              row.push(m('td', cardsEarned))
+            }
+            total = total != 0 ? total : null
+            rows.push(m('tr', row, m('td.total', total)))
+          }
+
+          const totalsRow = [m('td', 'Total')]
+          clanTotals.forEach((ele) => {
+            totalsRow.push(m('td', ele))
+          })
+
+          const totalTotals = clanTotals.reduce((a, b) => a + b, 0)
+          totalsRow.push(m('td', totalTotals))
+
+          rows.push(m('tr#collectionTotals.total', totalsRow))
+        }
+
+        return rows 
+      }
+
+      return m('tbody', renderCollectionsRows())
+    }
+
+    if (collections[0]) {
+      return m('table', {
+        id: 'collectionsTable',
+        class: 'table table-striped',
+        style: 'width:100%'
+        },
+        renderCollectionsTableHead(),
+        renderCollectionsTableBody()
+      )
+    }
   }
 
   const renderClanCollectionsView = () => {
     return m('div', {
       id: 'clan-collections',
-      class: 'tab-pane fade col-sm-12',
+      class: 'tab-pane fade col-sm-12 show active',
       role: 'tabpanel',
       'aria-labelledby': 'clan-collections-tab'
     }, renderCollectionsTable())
