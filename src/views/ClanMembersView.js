3c5db5b9a75ec28d43149b2002cc71eb6351f1dc
Gary Blaser <glblaser@gmail.com>
2020-03-25 23:30:23 -0500

    Added default desc sorting on column heading click

--- src/views/ClanMembersView.js
+++ src/views/ClanMembersView.js
@@ -39,7 +39,9 @@ export const ClanMembersView = ({ attrs: { clan, clanClient }}) => {
         "searchable": false,
         "orderable": false,
         "targets": 0
-      }],
+        },
+        { "orderSequence": [ "desc", "asc" ], "targets": [ 2, 3, 4, 5, 6, 7 ] }
+      ],
       "order": [[ 1, 'asc' ]],
       scrollY: 566,
       scrollX: true,
@@ -98,7 +100,7 @@ export const ClanMembersView = ({ attrs: { clan, clanClient }}) => {
   const renderClanMembersView = (clan) => {
     return m('div', {
       id: 'clan-members',
-      class: 'tab-pane fade col-sm-12',
+      class: 'tab-pane fade col-sm-12 show active',
       role: 'tabpanel',
       'aria-labelledby': 'clan-members-tab'
     }, 
